c409c9f31811c5fbd6819c59d6d334080262fe0d
NYA Zakaria <zakaria.nya@hetic.net>
2018-07-02 09:49:31 +0200

    fct: Ajout du style des cartes, fix les tri, et permet l'affichage du contenu de description des cartes depuis le typescript

--- src/app/main-dash/main-dash.component.ts
+++ src/app/main-dash/main-dash.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlatsService } from '../services/plats/plats.service';
+import {Food} from '../models/food.interface';
 
 @Component({
   selector: 'main-dash',
   templateUrl: './main-dash.component.html',
   styleUrls: ['./main-dash.component.css']
 })
-export class MainDashComponent {
+export class MainDashComponent implements OnInit {
   titre = 'Les traditions marocaines';
   cards = [
-    { title: 'a la carte', cols: 2, rows: 1 },
-    { title: 'couscous', cols: 1, rows: 1 },
-    { title: 'tajine', cols: 1, rows: 2 },
-    { title: 'pastilla', cols: 1, rows: 1 }
+    // { title: 'a la carte', cols: 2, rows: 1 },
+    { title: 'sucrée', cols: 2, rows: 1, id: 'suc' },
+    { title: 'salé', cols: 2, rows: 1, id: 'sal' },
+    { title: 'deserts', cols: 2, rows: 1, id: 'dess' }
   ];
+
+  legume: Food[];
+  sucree: Food[];
+  dessert: Food[];
+
+  constructor(private plat: PlatsService) { }
+
+  ngOnInit() {
+    this.dessert =   this.plat.getDessert();
+    this.sucree =   this.plat.getSucree();
+    this.legume =   this.plat.getLegume();
+  }
 }
